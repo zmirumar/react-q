1a8996c15e62336bd53ddbea5d224433585d4301
HasanGulomov <gulomovhasan194@gmail.com>
2025-10-05 15:07:30 +0500

    Hasan Legend is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import './App.css'
+import "./App.css";
+import Hasan from "./components/Hasan";
 
 function App() {
-
   const Learn = () => {
-    alert('Hello Git & GitHub')
-    console.log('Hello Git & GitHub')
-  }
+    alert("Hello Git & GitHub");
+    console.log("Hello Git & GitHub");
+  };
 
   return (
     <div>
+      <Hasan />
       <h1>Git & GitHub</h1>
       <button onClick={Learn}>Learn More</button>
       <button>Do not Learn</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
