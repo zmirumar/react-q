1da47b94c4c3382b4751c28c41c58d37f90eea02
Mirumar <bosithabibullaev@gmail.com>
2025-10-05 15:45:07 +0500

    Prime is comeing

--- src/components/Mirumar.tsx
+++ src/components/Mirumar.tsx
@@ -4,6 +4,7 @@ function Mirumar() {
   return (
     <div>
         <p>Truth:{Legend}</p>
+        <h1><a href="https://www.instagram.com/z.mirumar/">INSTAGRAM FROM MIRUMAR LEGEND</a></h1>
     </div>
   )
 }
