b98d0d14f76a2778c928de2d204f8ae427639df6
HasanGulomov <gulomovhasan194@gmail.com>
2025-10-06 16:06:50 +0500

    Xabibiloh

--- src/components/Hasan.tsx
+++ src/components/Hasan.tsx
@@ -1,4 +1,5 @@
-  
+import Xabi from "../assets/xabi.jpg";
+
 function Hasan() {
   const HasanButton = () => {
     alert("My brother Mirumar write codes better than me");
@@ -7,7 +8,10 @@ function Hasan() {
   };
   return (
     <div>
-
+      <div className="wrapper">
+        <h1>Xabibiloh: Nma Gap. Mirumar: Dinch</h1>
+        <img style={{ width: "500px", height: "500px" }} src={Xabi} alt="" />
+      </div>
       <h1>Hasan Legenda</h1>
 
       <button onClick={HasanButton}>Hasan's Button</button>
